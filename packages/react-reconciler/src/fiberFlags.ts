00151bfbe672a74a19f8abff66b5353c447ec300
huangchucai <425205839@qq.com>
2022-12-30 21:56:00 +0800

    feat: 修改flags

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -1,9 +1,9 @@
 export type Flags = number;
 
-export const NoFlags = 0b0000001; // 没有副作用
-export const Placement = 0b0000010; // 插入
-export const Update = 0b0000100; // 更新
-export const ChildDeletion = 0b0001000; // 删除
+export const NoFlags = 0b0000000; // 没有副作用
+export const Placement = 0b0000001; // 插入
+export const Update = 0b0000010; // 更新
+export const ChildDeletion = 0b0000100; // 删除
 
 /** 是否需要执行mutation阶段 */
 export const MutationMask = Placement | Update | ChildDeletion;
