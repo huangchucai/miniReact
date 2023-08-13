7e22f3c3db48cbe4b792e0f3ce419d2290de0974
huangchucai <425205839@qq.com>
2023-08-13 19:14:48 +0800

    feat: 实现suspense的工作流程（completeWork和commitWork)

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -4,12 +4,13 @@ export const NoFlags = 0b0000000; // 没有副作用
 export const Placement = 0b0000001; // 插入
 export const Update = 0b0000010; // 更新
 export const ChildDeletion = 0b0000100; // 删除
-
 export const PassiveEffect = 0b0001000; // 当前fiber上本次更新存在副作用
-
 export const Ref = 0b0010000; // ref
+export const Visibility = 0b0100000;
+
 /** 是否需要执行mutation阶段 */
-export const MutationMask = Placement | Update | ChildDeletion | Ref; //mutation阶段要执行的标志
+export const MutationMask =
+  Placement | Update | ChildDeletion | Ref | Visibility; //mutation阶段要执行的标志
 export const LayoutMask = Ref; // layout阶段要执行的标志
 
 /** 是否需要触发useEffect回调  */
